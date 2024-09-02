a45763dfe56c1916863a514e81620434d358d236
SharonJoseph.Enduri <SharonJoseph.Enduri@ust.com>
2024-09-02 13:04:31 +0530

    Created Models

---
+++ AuthenticationAPI/Models/RegisterBusiness.cs
@@ -0,0 +1,12 @@
+﻿namespace AuthenticationAPI.Models
+{
+    public class RegisterBusiness
+    {
+        public int vendor_id { get; set; }
+        public string vendor_name { get; set; }
+        public string vendor_email {  get; set; }
+        public string vendor_phone { get; set;}
+        public string vendor_address { get; set;}
+        public string password { get; set;}
+    }
+}
---
+++ AuthenticationAPI/Models/UserRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthenticationAPI.Models
+{
+    public class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+        public const string Vendor = "Vendor";
+    }
+}
