fcdd7d4caeb55551a4106c4f602a582ae6ba9838
SharonJoseph.Enduri <SharonJoseph.Enduri@ust.com>
2024-09-04 11:52:16 +0530

    Added nullable fields

--- AuthenticationAPI/Models/ApplicationUser.cs
+++ AuthenticationAPI/Models/ApplicationUser.cs
@@ -4,8 +4,8 @@ namespace AuthenticationAPI.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public string Address { get; set; }
-        public string State { get; set; }
-        public string PostalCode { get; set; }
+        public string? Address { get; set; }
+        public string? State { get; set; }
+        public string? PostalCode { get; set; }
     }
 }
