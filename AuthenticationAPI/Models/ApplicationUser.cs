4af4c8c8dacf47ddb3de5d2eb6abdcc264549091
SharonJoseph.Enduri <SharonJoseph.Enduri@ust.com>
2024-09-04 11:17:18 +0530

    Modified the fields in Application User

--- AuthenticationAPI/Models/ApplicationUser.cs
+++ AuthenticationAPI/Models/ApplicationUser.cs
@@ -5,5 +5,7 @@ namespace AuthenticationAPI.Models
     public class ApplicationUser : IdentityUser
     {
         public string Address { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
     }
 }
