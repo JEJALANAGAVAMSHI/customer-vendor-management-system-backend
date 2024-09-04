eb6a61707d52d8ad5f1bc2d23db86f868b30476a
SharonJoseph.Enduri <SharonJoseph.Enduri@ust.com>
2024-09-04 11:30:03 +0530

    added postal code and state

---
+++ AuthenticationAPI/Migrations/20240904054945_modified.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthenticationAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class modified : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCode",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "State",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "AspNetUsers");
+        }
+    }
+}
