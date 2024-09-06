4ecc435c4c156e00bcd6d7036eb621588e1e8c18
SharonJoseph.Enduri <SharonJoseph.Enduri@ust.com>
2024-09-06 15:56:53 +0530

    Added Register Customer Dto

---
+++ CustomerVendorApi/DTO/RegisterCustomerDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomerVendorApi.DTO
+{
+    public class RegisterCustomerDto
+    {
+        public string UserName { get; set; }      
+        public string Email { get; set; }      
+        public string PasswordHash { get; set; }      
+        public string PhoneNumber { get; set; }   
+        public string Address { get; set; }        
+        public string State { get; set; }         
+        public string PostalCode { get; set; }
+    }
+}
