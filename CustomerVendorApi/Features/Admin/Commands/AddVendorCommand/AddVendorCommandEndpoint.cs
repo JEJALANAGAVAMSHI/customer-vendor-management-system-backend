2187f293a44d9a78da23b4235c0235d3e41c89cd
SharonJoseph.Enduri <SharonJoseph.Enduri@ust.com>
2024-09-06 22:38:00 +0530

    Created add vendor functionality to admin

---
+++ CustomerVendorApi/Features/Admin/Commands/AddVendorCommand/AddVendorCommandEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using Carter;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerVendorApi.Features.Admin.Commands.AddVendorCommand
+{
+    public class AddVendorCommandEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/register-vendor", async ([FromBody] AddVendorCommand command, IMediator mediator) =>
+            {
+                var result = await mediator.Send(command);
+                if (result)
+                {
+                    return Results.Ok(new { Message = "Vendor registered successfully" });
+                }
+                return Results.StatusCode(500);
+            })
+            .WithName("RegisterVendor")
+            .WithTags("Vendor");
+        }
+    }
+}
