0faaca24287ad3b0738cf72b2799fece23d13aad
Jejala.Nagavamshi <jejala.nagavamshi@ust.com>
2024-09-07 16:55:50 +0530

    Implemented GetAllVendorsQueryEndpoint

--- CustomerVendorApi/Features/Admin/Queries/GetAllVendorsQuery/GetAllVendorsQueryEndpoint.cs
+++ CustomerVendorApi/Features/Admin/Queries/GetAllVendorsQuery/GetAllVendorsQueryEndpoint.cs
@@ -1,6 +1,23 @@
-﻿namespace CustomerVendorApi.Features.Admin.Queries.GetAllVendorsQuery
+﻿using Carter;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+
+namespace CustomerVendorApi.Features.Admin.Queries.GetAllVendorsQuery
 {
-    public class GetAllVendorsQueryEndpoint
+    public class GetAllVendorsQueryEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/vendors", [Authorize(Roles = "Admin")] async (HttpContext httpContext, IMediator mediator) =>
+            {
+                var query = new GetAllVendorsQuery();
+                var result = await mediator.Send(query);
+                return Results.Ok(result);
+            })
+            .WithName("GetAllVendors")
+            .WithTags("Vendors");
+        }
     }
 }
+
