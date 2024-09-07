8151b41c53564438b2ef016793e0aca2a083609d
Jejala.Nagavamshi <jejala.nagavamshi@ust.com>
2024-09-07 16:58:07 +0530

    Implemented GetAllVendorQueryHandler

--- CustomerVendorApi/Features/Admin/Queries/GetAllVendorsQuery/GetAllVendorsQueryHandler.cs
+++ CustomerVendorApi/Features/Admin/Queries/GetAllVendorsQuery/GetAllVendorsQueryHandler.cs
@@ -1,6 +1,55 @@
-﻿namespace CustomerVendorApi.Features.Admin.Queries.GetAllVendorsQuery
+﻿using CustomerVendorApi.DTO;
+using Mapster;
+using MediatR;
+using System.Text.Json;
+using System.Text;
+using System.Net.Http.Headers;
+
+namespace CustomerVendorApi.Features.Admin.Queries.GetAllVendorsQuery
 {
-    public class GetAllVendorsQueryHandler
+    public class GetAllVendorsQuery : IRequest<GetAllVendorsResponse>
     {
+
+    }
+
+    public class GetAllVendorsResponse
+    {
+        public IEnumerable<GetVendorDto> Vendors { get; set; }
+    }
+
+    public class GetAllVendorsQueryHandler : IRequestHandler<GetAllVendorsQuery, GetAllVendorsResponse>
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public GetAllVendorsQueryHandler(IHttpClientFactory httpClientFactory,
+            IHttpContextAccessor httpContextAccessor)
+        {
+            _httpClient = httpClientFactory.CreateClient("AuthService");
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<GetAllVendorsResponse> Handle(GetAllVendorsQuery request, CancellationToken cancellationToken)
+        {
+            var authHeader = _httpContextAccessor.HttpContext.Request.Headers["Authorization"].ToString();
+
+            if (string.IsNullOrEmpty(authHeader))
+            {
+                throw new HttpRequestException("Authorization header is missing.");
+            }
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authHeader.Replace("Bearer ", ""));
+
+            var response = await _httpClient.GetAsync("api/auth/vendors", cancellationToken);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var vendors = await response.Content.ReadFromJsonAsync<IEnumerable<GetVendorDto>>(cancellationToken: cancellationToken);
+                return new GetAllVendorsResponse { Vendors = vendors };
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+            throw new HttpRequestException($"Error fetching vendors: {response.StatusCode}, Content: {content}");
+        }
     }
 }
