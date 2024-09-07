dc47b5eebdcba2a4712a20110fd823065c933580
Jejala.Nagavamshi <jejala.nagavamshi@ust.com>
2024-09-07 14:53:20 +0530

    implemented delete vendor endpoint

--- CustomerVendorApi/Features/Admin/Commands/DeleteVendorCommand/DeleteVendorCommandEndpoint.cs
+++ CustomerVendorApi/Features/Admin/Commands/DeleteVendorCommand/DeleteVendorCommandEndpoint.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using Carter;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerVendorApi.Features.Admin.Commands.DeleteVendorCommand
+{
+    public class DeleteVendorCommandEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/delete-vendor/{vendorId}", async ([FromRoute] string vendorId, IMediator mediator) =>
+            {
+                var command = new DeleteVendorCommand { VendorId = vendorId };
+                var result = await mediator.Send(command);
+                if (result)
+                {
+                    return Results.Ok(new { Message = "Vendor deleted successfully" });
+                }
+                return Results.StatusCode(500);
+            })
+            .WithName("DeleteVendor")
+            .WithTags("Vendor");
+        }
+    }
+}
