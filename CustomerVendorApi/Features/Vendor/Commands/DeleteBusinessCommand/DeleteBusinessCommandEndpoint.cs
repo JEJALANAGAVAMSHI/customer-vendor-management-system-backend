df53aa98094f2d46968ad62dfc42577bd960b508
Jejala.Nagavamshi <jejala.nagavamshi@ust.com>
2024-09-08 14:43:48 +0530

    Implemented DeleteBusiness

--- CustomerVendorApi/Features/Vendor/Commands/DeleteBusinessCommand/DeleteBusinessCommandEndpoint.cs
+++ CustomerVendorApi/Features/Vendor/Commands/DeleteBusinessCommand/DeleteBusinessCommandEndpoint.cs
@@ -1,6 +1,21 @@
-﻿namespace CustomerVendorApi.Features.Vendor.Commands.DeleteBusinessCommand
+﻿using Carter;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CustomerVendorApi.Features.Vendor.Commands.DeleteBusinessCommand
 {
-    public class DeleteBusinessCommandEndpoint
+    public class DeleteBusinessCommandEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/businesses/{id}", [Authorize(Roles = "Vendor")] async (int id, IMediator mediator) =>
+            {
+                var command = new DeleteBusinessCommand { BusinessId = id };
+                await mediator.Send(command);
+                return Results.Ok("Deleted Successfully");  // Return NoContent (204) on successful deletion
+            })
+            .WithName("DeleteBusiness")
+            .WithTags("Businesses");
+        }
     }
 }
